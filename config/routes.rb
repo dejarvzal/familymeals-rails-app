235b7eb43eb7209e7d5df57c22b54631ce376e80
dejarvzal <diannejarvisj@gmail.com>
2020-09-17 19:57:28 -0700

    Add routes for login and signup

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,14 @@
 Rails.application.routes.draw do
   
   get '/' => 'sessions#welcome'
-
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
+  get '/signup' => 'users#new'
+  post '/signup' => 'users#create'
   resources :ingredients
   resources :recipes
   resources :meals
   resources :users
-
 
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
