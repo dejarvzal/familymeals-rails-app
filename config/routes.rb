880726f307f25c9e5a1dace94c7aad3e94501fd2
dejarvzal <diannejarvisj@gmail.com>
2020-09-17 18:45:45 -0700

    Create Sessions controller, update routes, welcome view page

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
+  
+  get '/' => 'sessions#welcome'
   resources :ingredients
   resources :recipes
   resources :meals
   resources :users
+
+
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
