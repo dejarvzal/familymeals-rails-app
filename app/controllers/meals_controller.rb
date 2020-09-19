6cd3ceb4115c7ce49aaad98876ed2a68730a9855
dejarvzal <diannejarvisj@gmail.com>
2020-09-18 23:02:21 -0700

    Add create action for new meal

--- app/controllers/meals_controller.rb
+++ app/controllers/meals_controller.rb
@@ -2,6 +2,22 @@ class MealsController < ApplicationController
 
     def new
         @meal = Meal.new
+        @meal.build_recipe
     end
 
+    def create
+        @meal = Meal.new(meal_params)
+        if @meal.save!
+         redirect_to meal_path(@meal)
+        else
+         render :new
+        end
+    end
+
+
+    private
+
+    def meal_params
+        params.require(:meal).permit(:name, :category, :date, :notes, :recipe_id, recipe_attributes: [:name, :is_vegan, :instruction] )
+    end
 end
