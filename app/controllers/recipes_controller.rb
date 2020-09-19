9db5478d3914c65ec4f840802578b1c16cbc478b
dejarvzal <diannejarvisj@gmail.com>
2020-09-19 16:59:48 -0700

    Add create action for recipe controller

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -3,4 +3,21 @@ class RecipesController < ApplicationController
     def new
         @recipe = Recipe.new
     end
+
+    def create
+        @recipe = Recipe.new(recipe_params)
+        if @recipe.save
+         redirect_to recipe_path(@recipe) #, notice: "recipe saved!"
+        else
+         flash[:eror] = "Your recipe didn't save correctly."
+         render :new
+        end
+    end
+
+    private
+
+    def recipe_params
+        params.require(:recipe).permit(:name, :instruction, :is_vegan)
+    end
+
 end
