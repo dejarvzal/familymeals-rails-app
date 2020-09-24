657cae902aad874bd201483f63817611dcc20918
dejarvzal <diannejarvisj@gmail.com>
2020-09-23 22:34:56 -0700

    Fix association for recipe in meal

--- app/models/meal.rb
+++ app/models/meal.rb
@@ -1,6 +1,9 @@
 class Meal < ApplicationRecord
     belongs_to :user
-    belongs_to :recipe
+    has_many :recipes
 
-    accepts_nested_attributes_for :recipe
+    accepts_nested_attributes_for :recipes
+    
 end
+
+#changed the association from belongs_to :recipe to has_many through
