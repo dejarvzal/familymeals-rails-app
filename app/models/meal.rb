0b5af4ec3fd470eefbfe4abc68e480897b610ee6
dejarvzal <diannejarvisj@gmail.com>
2020-09-26 09:58:58 -0700

    Update validations for creating a meal

--- app/models/meal.rb
+++ app/models/meal.rb
@@ -4,7 +4,12 @@ class Meal < ApplicationRecord
     has_many :ingredients, through: :recipes
 
     accepts_nested_attributes_for :recipes
+
+    validates :name, uniqueness: true, presence: true
+    validates :category, :date, :recipes, presence: true
     
 end
+
+# need to be logged in to create a meal - need validation
 
 #changed the association from belongs_to :recipe to has_many through
