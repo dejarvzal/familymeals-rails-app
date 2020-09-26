cb9a3801f027dcd4e13ff749b94e2c6e65e1fe8b
dejarvzal <diannejarvisj@gmail.com>
2020-09-26 10:27:37 -0700

    Update validation for creating an ingredient

--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -1,5 +1,10 @@
 class Ingredient < ApplicationRecord
-  has_many :recipes
+  has_many :recipes 
   has_many :meals, through: :recipes
 
+  validates :food_item, uniqueness: true, presence: true
+  # validates :amount, presence: true <--may not not need to be required
+
 end
+
+#should an ingredient only belong to one recipe? revisit
