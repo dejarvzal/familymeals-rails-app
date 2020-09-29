fd09b9dfd21ab18848796ac0cd476f0f1629f0f6
dejarvzal <diannejarvisj@gmail.com>
2020-09-28 22:51:47 -0700

    Setup seed file, need some modification

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,37 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+models = [User, Meal, Recipe, Ingredients]
+
+#Reset All Table Data
+
+models.each do |model|
+    model.destroy_all
+    ActiveRecord::Base.connection.reset_sequence!("#{model.to_s.downcase + "s"}", "id", squence = nil)
+end
+
+User.create!(
+    name: "Dianne",
+    email_address: "dianne@gmail.com"
+    password: "123"
+)
+
+User.create!(
+    name: "Ebony",
+    email_address: "ebony@gmail.com"
+    password: "123"
+)
+
+p "#{User.count} Users created!"
+
+10.times do 
+    Recipe.create!(
+        meal_id: Faker::Number.between(from Meal.first.id, to: Meal.last.id),
+        instruction: Faker::Hipster.paragraphs,
+        is_vegan: Faker::Boolean.boolean,
+        name: Faker::Hipster.unique.word
+    )
+end
+
+p "#{Recipe.count} Users created!"
