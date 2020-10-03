b09c801eb1974697fe7ce84f0c05dff589421638
dejarvzal <diannejarvisj@gmail.com>
2020-10-03 15:46:16 -0700

    Update relationship to delete dependents when meal is deleted

--- app/models/meal.rb
+++ app/models/meal.rb
@@ -1,15 +1,24 @@
 class Meal < ApplicationRecord
-    belongs_to :user
-    has_many :recipes
+    belongs_to :user, optional: true
+    has_many :recipes,  :dependent => :destroy
     has_many :ingredients, through: :recipes
 
     accepts_nested_attributes_for :recipes
 
-    validates :name, uniqueness: true, presence: true
-    validates :category, :date, :recipes, presence: true
-    
-end
+    validates :name, :category, :date, presence: true
+    # validates :category, :date, :recipes, presence: true
+   
+    def recipes_attributes=(attributes)
+        recipe = Recipe.find_or_create_by(attributes) if !attributes['name'].nil?
+    end
 
-# need to be logged in to create a meal - need validation
+    # def not_a_duplicate
+    #     if Meal.find_by(name: name, date: date)
+    #         errors.add(:name, "has already been added for that date!")
+    #     end
+    # end
+
+end
+#meal cannot be duplicated for the same date
 
 #changed the association from belongs_to :recipe to has_many through
