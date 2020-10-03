0facb83ea2a378c1e998158bac8920bbf5e3f96b
dejarvzal <diannejarvisj@gmail.com>
2020-10-03 15:43:59 -0700

    Update seed file with sample data

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,7 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-models = [User, Meal, Recipe, Ingredients]
+models = [User, Meal, Recipe, Ingredient]
 
 #Reset All Table Data
 
@@ -17,25 +17,82 @@ end
 
 User.create!(
     name: "Dianne",
-    email_address: "dianne@gmail.com"
+    email_address: "dianne@gmail.com",
     password: "123"
 )
 
 User.create!(
     name: "Ebony",
-    email_address: "ebony@gmail.com"
+    email_address: "ebony@gmail.com",
     password: "123"
 )
 
 p "#{User.count} Users created!"
 
-10.times do 
-    Recipe.create!(
-        meal_id: Faker::Number.between(from Meal.first.id, to: Meal.last.id),
-        instruction: Faker::Hipster.paragraphs,
-        is_vegan: Faker::Boolean.boolean,
-        name: Faker::Hipster.unique.word
+3.times do 
+    Meal.create!(
+        # meal_id: Faker::Number.between(from Meal.first.id, to: Meal.last.id),
+        # category: Faker::Food.description,
+        category: "Breakast",
+        name: Faker::Food.unique.dish,
+        date: Faker::Date.backward(days: 30),
+        notes: Faker::Food.description
     )
 end
 
-p "#{Recipe.count} Users created!"
+3.times do 
+    Meal.create!(
+        # meal_id: Faker::Number.between(from Meal.first.id, to: Meal.last.id),
+        # category: Faker::Food.description,
+        category: "Lunch",
+        name: Faker::Food.unique.dish,
+        date: Faker::Date.backward(days: 30),
+        notes: Faker::Food.description
+    )
+end
+
+3.times do 
+    Meal.create!(
+        # meal_id: Faker::Number.between(from Meal.first.id, to: Meal.last.id),
+        # category: Faker::Food.description,
+        category: "Brunch",
+        name: Faker::Food.unique.dish,
+        date: Faker::Date.backward(days: 30),
+        notes: Faker::Food.description
+    )
+end
+
+3.times do 
+    Meal.create!(
+        # meal_id: Faker::Number.between(from Meal.first.id, to: Meal.last.id),
+        # category: Faker::Food.description,
+        category: "Dinner",
+        name: Faker::Food.unique.dish,
+        date: Faker::Date.backward(days: 30),
+        notes: Faker::Food.description
+    )
+end
+
+p "#{Meal.count} Meals created!"
+
+10.times do 
+    Recipe.create!(
+        meal_id: Faker::Number.between(from: Meal.first.id, to: Meal.last.id),
+        instruction: Faker::Food.description,
+        is_vegan: Faker::Boolean.boolean,
+        name: Faker::Food.unique.dish
+    )
+end
+
+p "#{Recipe.count} Recipes created!"
+
+10.times do 
+    Ingredient.create!(
+        recipe_id: Faker::Number.between(from: Recipe.first.id, to: Recipe.last.id),
+        food_item: Faker::Food.ingredient,
+        amount: Faker::Number.between(from: 2.00, to: 10.00),
+        measurement: Faker::Food.measurement    
+    )
+end
+
+p "#{Ingredient.count} Ingredients created!"
