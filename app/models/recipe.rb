e5a90a36f9355e0f88b3edbf57dff4db5fa92d17
dejarvzal <diannejarvisj@gmail.com>
2020-10-03 15:45:36 -0700

    Update relationship to delete dependents when recipe is deleted

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,7 +1,7 @@
 class Recipe < ApplicationRecord
     has_many :users, through: :meals
     belongs_to :meal, optional: true
-    has_many :ingredients
+    has_many :ingredients, :dependent => :destroy
   
     # validates :name, presence: true
     # validates :name, :instruction, presence: true
