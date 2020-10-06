48affaf98c6888cba8ae28a6c8cf10a54812ad32
dejarvzal <diannejarvisj@gmail.com>
2020-10-05 21:51:26 -0700

    Update route with google oauth callback

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,8 @@ Rails.application.routes.draw do
   post '/signup' => 'users#create'
   delete '/logout' => 'sessions#destroy'
 
+  get 'auth/google_oauth2/callback' => 'sessions#omniauth'
+
  
   resources :ingredients
   resources :recipes
