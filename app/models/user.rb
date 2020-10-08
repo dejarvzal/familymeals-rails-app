01c05ed5c7853bbbeffa995a6a6a303c2b5f9f44
dejarvzal <diannejarvisj@gmail.com>
2020-10-07 23:38:51 -0700

    Update create_by_google_auth method to include required fields

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,8 +7,10 @@ class User < ApplicationRecord
     has_secure_password
 
     def self.create_by_google_omniauth(auth)
-        self.find_or_create_by(name: auth[:info][:email]) do |u|
-        u.password = SecureRandom.hex
+        self.find_or_create_by(name: auth[:info][:name]) do |u|
+            u.email_address = auth.info.email
+            u.password = SecureRandom.hex
+
         end
     end
 end
